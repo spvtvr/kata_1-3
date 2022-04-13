99c29f7c9e8c3c16663c38161c4938f0c6e639ab
spvtvr <102229185+spvtvr@users.noreply.github.com>
2022-04-13 16:45:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 16,
+    width: 240,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+
